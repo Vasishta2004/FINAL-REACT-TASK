2cd8a580552f42c23906ab57c59ccb4d8a9fa647
vasishta <vasivasishta4@gmail.com>
2026-08-18 00:39:06 +0530

    Add CarContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { CarProvider } from "./Context/CarContext";
-import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { CarProvider } from "./context/CarContext.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <CarProvider>
-        <App />
-      </CarProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <CarProvider>
+      <App />
+    </CarProvider>
+  </StrictMode>
 );
